3be340e54b1b620013eae2741b9eeec21a8c3122
Dabnis <jonathan@dabnis.com>
2024-10-06 22:08:39 +0100

    Testing multiple add_knowledge calls

--- main.py
+++ main.py
@@ -3,18 +3,25 @@ import time
 from kg.kg import KnowledgeGraph
 
 kg = KnowledgeGraph()
+# Test set
+# bodies = ["The Universe", "The Solar System"]
 
-# bodies = ["The Universe", "The Solar System", "Planet Pluto", "Planet Venus", "Planet Earth", "Planet Mars", "Planet Jupiter", "Planet Saturn", "The moons of Saturn", "The moons of Saturn", "Planet Neptune", "Planet Uranus", "The Sun of our solar system"]
-bodies = ["The Universe", "The Solar System"]
+bodies = ["The Universe", "The Solar System", "Planet Pluto", "Planet Venus", "Planet Earth", "Planet Mars", "Planet Jupiter", "Planet Saturn", "The moons of Saturn", "The moons of Saturn", "Planet Neptune", "Planet Uranus", "The Sun of our solar system"]
+
 # Build a knowledge graph
-for body in bodies:
-    start_time = time.time()
-    print(f"Processing '{body}'")
-    # Replace 'kg.add_knowledge("The Universe")' with your actual code here
-    kg.add_knowledge(body)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Time taken: {elapsed_time:.2f} seconds")
+# for body in bodies:
+#     start_time = time.time()
+#     print(f"Processing '{body}'")
+#     # Replace 'kg.add_knowledge("The Universe")' with your actual code here
+#     kg.add_knowledge(body)
+#     end_time = time.time()
+#     elapsed_time = end_time - start_time
+#     print(f"Time taken: {elapsed_time:.2f} seconds")
 
-response = kg.interrogate("Of all of our solar system planets and moons, which is the most likely to have some form of life?")
-print(response)
+# kg.add_knowledge("Astronomy and mythology")
+# kg.add_knowledge("Known planets outside of our solar system")
+
+
+# print("You asked:", text_input)
+response = kg.interrogate("Explain what solar mass is, and give the solar mass of the earth")
+print("My response:", response)
