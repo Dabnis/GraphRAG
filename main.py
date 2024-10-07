2b6f1e157cd52c326ef33f5add8e0668303c7e10
Dabnis <jonathan@dabnis.com>
2024-10-07 21:40:05 +0100

    Update for moto :)

--- main.py
+++ main.py
@@ -4,21 +4,18 @@ from kg.kg import KnowledgeGraph
 
 kg = KnowledgeGraph()
 # Test set
-bodies = ["The Universe", "The Solar System", "Planet Jupiter"]
+# bodies = ["The Universe", "The Solar System", "Planet Jupiter"]
 
-# bodies = ["The Universe", "The Solar System", "Planet Pluto", "Planet Venus", "Planet Earth", "Planet Mars", "Planet Jupiter", "Planet Saturn", "The moons of Saturn", "The moons of Saturn", "Planet Neptune", "Planet Uranus", "The Sun", "Astronomy and mythology", "Known planets outside our solar system"]
+bodies = ["The Universe", "The Solar System", "Planet Pluto", "Planet Venus", "Planet Earth", "Planet Mars", "Planet Jupiter", "Planet Saturn", "The moons of Saturn", "The moons of Saturn", "Planet Neptune", "Planet Uranus", "The Sun", "Astronomy and mythology", "Known planets outside our solar system"]
 
 # Build a knowledge graph
-# for body in bodies:
-#     start_time = time.time()
-#     print(f"Processing '{body}'")
-#     # Replace 'kg.add_knowledge("The Universe")' with your actual code here
-#     kg.add_knowledge(body)
-#     end_time = time.time()
-#     elapsed_time = end_time - start_time
-#     print(f"Time taken: {elapsed_time:.2f} seconds")
+for body in bodies:
+    start_time = time.time()
+    print(f"Processing '{body}'")
+    kg.add_knowledge(body)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Time taken: {elapsed_time:.2f} seconds")
 
-
-# print("You asked:", text_input)
 response = kg.interrogate("What size is the universe")
 print("My response:", response)
