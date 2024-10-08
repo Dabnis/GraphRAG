ca49b21acc9f380559661a527f3be0f504aa655c
Dabnis <jonathan@dabnis.com>
2024-10-08 13:41:30 +0100

    Refactoring

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ kg = KnowledgeGraph()
 
 if add_knowledge:
     # Build a knowledge graph about our solar systems.
-    bodies = ["The Solar System", "Planet Pluto", "Planet Venus", "Planet Earth", "Planet Mars", "Planet Jupiter", "Planet Saturn", "The moons of Saturn", "The moons of Saturn", "Planet Neptune", "Planet Uranus", "The Sun"]
+    core_subject = ["The Solar System", "The Planet Pluto", "The Planet Venus", "The Planet Earth", "The Planet Mars",
+              "The Planet Jupiter", "The Planet Saturn", "The moons of Saturn", "The moons of Saturn",
+              "The Planet Neptune", "The Planet Uranus", "The Sun"]
 
-    # Build a knowledge graph
-    for body in bodies:
+    # Build the knowledge graph
+    for subject in core_subject:
         start_time = time.time()
-        print(f"Processing '{body}'")
-        kg.add_knowledge(body)
+        print(f"Processing '{subject}'")
+        kg.add_knowledge(subject)
         end_time = time.time()
         elapsed_time = end_time - start_time
         print(f"Time taken: {elapsed_time:.2f} seconds")
