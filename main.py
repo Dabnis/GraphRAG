6aacf6b967496cb6056e6ef382d70c4b285cbd43
Dabnis <jonathan@dabnis.com>
2024-10-08 13:16:58 +0100

    Simplified load knowledge functionality

--- main.py
+++ main.py
@@ -2,22 +2,25 @@ import time
 
 from kg.kg import KnowledgeGraph
 
+add_knowledge = True
+
 kg = KnowledgeGraph()
 # Test set
-# bodies = ["The Universe", "The Solar System", "Planet Jupiter"]
+# bodies = ["LLM context compression", "LLM context size", "neo4j", "cypher queries"]
 
-bodies = ["The Solar System", "Planet Pluto", "Planet Venus", "Planet Earth", "Planet Mars", "Planet Jupiter", "Planet Saturn", "The moons of Saturn", "Planet Neptune", "Planet Uranus", "The Sun"]
+if add_knowledge:
+    # Build a knowledge graph about our solar systems.
+    bodies = ["The Solar System", "Planet Pluto", "Planet Venus", "Planet Earth", "Planet Mars", "Planet Jupiter", "Planet Saturn", "The moons of Saturn", "The moons of Saturn", "Planet Neptune", "Planet Uranus", "The Sun"]
 
-# Build a knowledge graph
-# for body in bodies:
-#     start_time = time.time()
-#     print(f"Processing '{body}'")
-#     kg.add_knowledge(body)
-#     end_time = time.time()
-#     elapsed_time = end_time - start_time
-#     print(f"Time taken: {elapsed_time:.2f} seconds")
+    # Build a knowledge graph
+    for body in bodies:
+        start_time = time.time()
+        print(f"Processing '{body}'")
+        kg.add_knowledge(body)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Time taken: {elapsed_time:.2f} seconds")
 
-# kg.add_knowledge( "The Planet Venus")
 
-response = kg.interrogate("Could humans live on Venus")
+response = kg.interrogate("Apart from Earth what is the most likely planet that humans could inhabit")
 print("My response:", response)
