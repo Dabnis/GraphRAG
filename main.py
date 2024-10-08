b59d74715080179fd12b3a924959746dc1f1c78d
Dabnis <jonathan@dabnis.com>
2024-10-08 11:35:49 +0100

    Placed constraints on nodes and properties returned by LLMGraphTransformer. See mentions docs in code

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ kg = KnowledgeGraph()
 # Test set
 # bodies = ["The Universe", "The Solar System", "Planet Jupiter"]
 
-bodies = ["The Universe", "The Solar System", "Planet Pluto", "Planet Venus", "Planet Earth", "Planet Mars", "Planet Jupiter", "Planet Saturn", "The moons of Saturn", "The moons of Saturn", "Planet Neptune", "Planet Uranus", "The Sun", "Astronomy and mythology", "Known planets outside our solar system"]
+bodies = ["The Solar System", "Planet Pluto", "Planet Venus", "Planet Earth", "Planet Mars", "Planet Jupiter", "Planet Saturn", "The moons of Saturn", "Planet Neptune", "Planet Uranus", "The Sun"]
 
 # Build a knowledge graph
-for body in bodies:
-    start_time = time.time()
-    print(f"Processing '{body}'")
-    kg.add_knowledge(body)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Time taken: {elapsed_time:.2f} seconds")
+# for body in bodies:
+#     start_time = time.time()
+#     print(f"Processing '{body}'")
+#     kg.add_knowledge(body)
+#     end_time = time.time()
+#     elapsed_time = end_time - start_time
+#     print(f"Time taken: {elapsed_time:.2f} seconds")
 
-response = kg.interrogate("What size is the universe")
+# kg.add_knowledge( "The Planet Venus")
+
+response = kg.interrogate("Could humans live on Venus")
 print("My response:", response)
